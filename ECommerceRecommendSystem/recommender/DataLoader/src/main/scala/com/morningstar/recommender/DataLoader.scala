443dc90307f26f49dcc3e3289674fd760a05b51c
GaryMK <49356298+GaryMK@users.noreply.github.com>
2021-01-02 16:15:55 +0800

    update copyright

---
+++ ECommerceRecommendSystem/recommender/DataLoader/src/main/scala/com/morningstar/recommender/DataLoader.scala
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2021-$year, GaryMK  All Rights Reserved.
+ * ProjectName:  ECommerceRecommendSystem
+ * FileName:  DataLoader.scala
+ * Author:  GaryMK
+ * Date:  2021/1/2 下午4:15
+ * Version:  1.0
+ * LastModified:  2021/1/2 下午4:15
+ */
+
+package com.morningstar.recommender
+
+/**
+ * @author: GaryMK
+ * @EMAIL: chenxingmk@gmail.com
+ * @Date: 2021/1/2$ 16:15
+ * @Description:
+ */
+object DataLoader {
+
+}
